126c20976044fc850226d73d4dd647545bc5041d
Bovojon <bovojon@gmail.com>
2020-08-25 14:13:38 -0700

    Add rest-framework to settings.py

--- boilerproject/settings.py
+++ boilerproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     # Custom
     'boilerapi.apps.BoilerapiConfig',
@@ -53,6 +54,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boilerproject.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 TEMPLATES = [
     {
